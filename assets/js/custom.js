46a97e4dec44384a97b4ccadc7002c76921d0014
nazifa22 <nazifarashid1822@gmail.com>
2019-03-14 01:49:27 +0800

    started testimonial

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,8 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if ($(window).scrollTop() >= 100) {
-            $('header > nav').removeClass('bg-transparent');
-        }
-        else {
-            $('header > nav').addClass('bg-transparent');
-        }
-    });
+$('.counter').counterUp({
+    delay: 10,
+    time: 2000
 });
+
 
 
 $('body').scrollspy({ target: '#navbar-example', offset 100});
@@ -15,3 +10,19 @@ $('body').scrollspy({ target: '#navbar-example', offset 100});
 $('[data-spy="scroll"]').each(function () {
     var $spy = $(this).scrollspy('refresh');
 });
+
+wow = new WOW(
+    {
+    animateClass: 'animated',
+    offset:       100,
+    callback:     function(box) {
+        console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+    }
+    }
+);
+wow.init();
+document.getElementById('moar').onclick = function() {
+    var section = document.createElement('section');
+    section.className = 'section--purple wow fadeInDown';
+    this.parentNode.insertBefore(section, this);
+};
